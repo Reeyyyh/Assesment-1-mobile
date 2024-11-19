d48259e3b156c9ce95990e6898adafe41b04ac26
Reeyyyh <rhnlnv@gmail.com>
2024-11-20 03:07:00 +0700

    output audio pada profile

--- lib/app/modules/4_profile/controllers/profile_controller.dart
+++ lib/app/modules/4_profile/controllers/profile_controller.dart
@@ -3,59 +3,67 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:get/get.dart';
 import 'package:hotel_app/app/modules/Aunt/views/login_view.dart';
-import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 
 class ProfileController extends GetxController {
   var imagePath = ''.obs;
-  var imageFile;
   var userName = ''.obs;
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final ImagePicker _picker = ImagePicker();
-  
+
   // Inisialisasi flutter_tts
   final FlutterTts flutterTts = FlutterTts();
 
+  // Fungsi untuk mengucapkan teks menggunakan TTS
+  Future<void> _speak(String text) async {
+    try {
+      await flutterTts.awaitSpeakCompletion(true);
+      await flutterTts.setLanguage("en-US");
+      await flutterTts.setPitch(1);
+      await flutterTts.speak(text);
+    } catch (e) {
+      print("Error saat menggunakan TTS: $e");
+    }
+  }
+
+  // Fungsi untuk mengambil data user dan memutar sapaan
   void fetchUserName() async {
     User? user = _auth.currentUser;
     if (user != null) {
       DocumentSnapshot doc = await _firestore.collection('users').doc(user.uid).get();
       userName.value = doc['namaUser'];
       imagePath.value = doc['profilePicture'] ?? '';
-      _speakHello(userName.value); // Menyapa setelah nama diambil
+      // Delay untuk memastikan TTS siap digunakan
+      Future.delayed(Duration(milliseconds: 500), () {
+        _speak("Hello, ${userName.value}");
+      });
     }
   }
 
-  // Fungsi untuk mengucapkan 'Hello, <username>'
-  void _speakHello(String username) async {
-    await flutterTts.setLanguage("en-US"); // Set bahasa (opsional)
-    await flutterTts.setPitch(1); // Set pitch suara (opsional)
-    await flutterTts.speak("Hello, $username"); // Mengucapkan teks
-  }
-
+  // Fungsi untuk memutar sapaan secara manual
   Future<void> speakHello() async {
     if (userName.value.isNotEmpty) {
-      await flutterTts.setLanguage("en-US"); // Set bahasa (opsional)
-    await flutterTts.setPitch(1); // Set pitch suara (opsional)
-    await flutterTts.speak("Hello"); // Mengucapkan teks
+      await _speak("Hello, ${userName.value}");
     }
   }
 
+  // Fungsi untuk logout
   void logOut() async {
     await _auth.signOut();
     Get.offAll(LoginView());
   }
 
+  // Fungsi untuk memilih gambar profil
   Future<void> pickImage() async {
     final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
     if (pickedFile != null) {
-      imageFile = File(pickedFile.path);
       imagePath.value = pickedFile.path;
       await _saveProfilePicture();
     }
   }
 
+  // Fungsi untuk menyimpan foto profil ke Firestore
   Future<void> _saveProfilePicture() async {
     User? user = _auth.currentUser;
     if (user != null) {
@@ -65,8 +73,8 @@ class ProfileController extends GetxController {
     }
   }
 
+  // Fungsi untuk menghapus gambar profil
   Future<void> removeImage() async {
-    imageFile = null;
     imagePath.value = '';
     User? user = _auth.currentUser;
     if (user != null) {
@@ -76,6 +84,7 @@ class ProfileController extends GetxController {
     }
   }
 
+  // Fungsi untuk mengupdate nama pengguna
   Future<void> updateUserName(String newName) async {
     User? user = _auth.currentUser;
     if (user != null) {
--- lib/app/modules/4_profile/views/profile_view.dart
+++ lib/app/modules/4_profile/views/profile_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:hotel_app/app/modules/4_profile/views/edit_profile_view.dart';
 import '../controllers/profile_controller.dart';
 import 'dart:io';
 
@@ -15,7 +14,7 @@ class ProfileView extends StatelessWidget {
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(90.0),
         child: ClipPath(
-          clipper: CustomAppBarClipper(), // Menggunakan Custom Clipper
+          clipper: CustomAppBarClipper(),
           child: Container(
             decoration: const BoxDecoration(
               gradient: LinearGradient(
@@ -25,22 +24,17 @@ class ProfileView extends StatelessWidget {
               ),
             ),
             child: AppBar(
-              title: const Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    'Profile',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
+              title: const Text(
+                'Profile',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
               ),
               centerTitle: true,
               elevation: 0,
-              backgroundColor: Colors.transparent, // Membuat latar belakang transparan
+              backgroundColor: Colors.transparent,
             ),
           ),
         ),
@@ -50,87 +44,81 @@ class ProfileView extends StatelessWidget {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-              // Header tanpa dekorasi
-              SizedBox(
-                width: 250,
-                height: 250,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        if (controller.imagePath.value.isEmpty) {
-                          controller.pickImage();
-                        } else {
-                          showDialog(
-                            context: context,
-                            builder: (context) {
-                              return AlertDialog(
-                                title: const Text('Foto Profil'),
-                                content: const Text(
-                                    'Pilih tindakan untuk foto profil Anda.'),
-                                actions: [
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                      controller.removeImage();
-                                    },
-                                    child: const Text('Hapus Foto'),
-                                  ),
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                      controller.pickImage();
-                                    },
-                                    child: const Text('Ganti Foto'),
-                                  ),
-                                ],
-                              );
-                            },
-                          );
-                        }
+              // Gambar Profil
+              GestureDetector(
+                onTap: () {
+                  if (controller.imagePath.value.isEmpty) {
+                    controller.pickImage();
+                  } else {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: const Text('Foto Profil'),
+                          content: const Text(
+                              'Pilih tindakan untuk foto profil Anda.'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                                controller.removeImage();
+                              },
+                              child: const Text('Hapus Foto'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                                controller.pickImage();
+                              },
+                              child: const Text('Ganti Foto'),
+                            ),
+                          ],
+                        );
                       },
-                      child: CircleAvatar(
-                        radius: 70,
-                        backgroundColor: const Color.fromARGB(191, 94, 92, 92),
-                        backgroundImage: controller.imagePath.value.isNotEmpty
-                            ? FileImage(File(controller.imagePath.value))
-                            : null,
-                        child: controller.imagePath.value.isEmpty
-                            ? const Icon(
-                                Icons.person,
-                                size: 70,
-                                color: Colors.white,
-                              )
-                            : null,
-                      ),
-                    ),
-                    const SizedBox(height: 15),
-                    // Nama Pengguna
-                    Text(
-                      "Hello ${controller.userName.value}",
-                      style: const TextStyle(
-                        fontSize: 26,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ],
+                    );
+                  }
+                },
+                child: CircleAvatar(
+                  radius: 70,
+                  backgroundColor: const Color.fromARGB(191, 94, 92, 92),
+                  backgroundImage: controller.imagePath.value.isNotEmpty
+                      ? FileImage(File(controller.imagePath.value))
+                      : null,
+                  child: controller.imagePath.value.isEmpty
+                      ? const Icon(
+                          Icons.person,
+                          size: 70,
+                          color: Colors.white,
+                        )
+                      : null,
+                ),
+              ),
+              const SizedBox(height: 15),
+
+              // Nama Pengguna dan Tombol Sapaan
+              Text(
+                "Hello ${controller.userName.value}",
+                style: const TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
                 ),
               ),
               const SizedBox(height: 10),
-
-              // Garis Pemisah
-              Divider(color: Colors.grey.shade300, thickness: 2),
-
+              ElevatedButton.icon(
+                onPressed: () {
+                  controller.speakHello();
+                },
+                icon: const Icon(Icons.volume_up),
+                label: const Text("Play Greeting"),
+              ),
               const SizedBox(height: 30),
+
               // Menu Pengaturan
               ListTile(
                 leading: const Icon(Icons.person),
                 title: const Text('Personal Info'),
-                onTap: () {
-                  Get.to(() => EditProfileView());
-                },
+                onTap: () {},
               ),
               ListTile(
                 leading: const Icon(Icons.security),
@@ -140,10 +128,7 @@ class ProfileView extends StatelessWidget {
               ListTile(
                 leading: const Icon(Icons.settings),
                 title: const Text('Account Settings'),
-                onTap: () {
-                  controller.speakHello();
-                  print("tombol di temkan");
-                },
+                onTap: () {},
               ),
               ListTile(
                 iconColor: Colors.red,
@@ -153,9 +138,6 @@ class ProfileView extends StatelessWidget {
                 onTap: () {
                   Get.defaultDialog(
                     radius: 10,
-                    backgroundColor: Colors.grey[50],
-                    confirmTextColor: Colors.green,
-                    cancelTextColor: Colors.red,
                     title: "Konfirmasi",
                     middleText: "Apakah Anda yakin ingin keluar?",
                     textConfirm: "Ya",
@@ -170,7 +152,6 @@ class ProfileView extends StatelessWidget {
                   );
                 },
               ),
-              const SizedBox(height: 10),
             ],
           ),
         );
@@ -183,11 +164,10 @@ class CustomAppBarClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     Path path = Path();
-    path.lineTo(0, size.height - 30); // Turun ke titik sebelum melengkung (disesuaikan)
-    path.quadraticBezierTo(size.width / 2, size.height, size.width, size.height - 30); // Lengkungkan ke bawah (disesuaikan)
-    path.lineTo(size.width, 0); // Garis ke atas
-    path.lineTo(0, 0); // Kembali ke titik awal
-    path.close(); // Menutup path
+    path.lineTo(0, size.height - 30);
+    path.quadraticBezierTo(size.width / 2, size.height, size.width, size.height - 30);
+    path.lineTo(size.width, 0);
+    path.close();
     return path;
   }
 
